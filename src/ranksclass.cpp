9cfc60e41ac392c1d62df83beb0ad86aeac65ea1
zeynys <159999156+zeynys@users.noreply.github.com>
2025-05-03 21:31:01 +0300

    fix: argument order

--- src/ranksclass.cpp
+++ src/ranksclass.cpp
@@ -10,13 +10,13 @@ void SetupScripting(EContext* ctx)
 
     ADD_CLASS_FUNCTION("RanksClass", "GetRankPoints", [](FunctionContext* context, ClassData* data) -> void {
         int playerid = context->GetArgumentOr<int>(0, 0);
-        int player_points = context->GetArgumentOr<int>(0, 0);
+        int player_points = context->GetArgumentOr<int>(1, 0);
         points[playerid] = player_points;
     });
 
     ADD_CLASS_FUNCTION("RanksClass", "GetRankSkillID", [](FunctionContext* context, ClassData* data) -> void {
         int playerid = context->GetArgumentOr<int>(0, 0);
-        int player_rank = context->GetArgumentOr<int>(0, 0);
+        int player_rank = context->GetArgumentOr<int>(1, 0);
         rank[playerid] = player_rank;
     });
 
